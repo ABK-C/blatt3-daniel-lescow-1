f7a1f333d0399f81e2f86dad97b9eb62f8fbcf43
daniellescow <lescowdaniel@gmail.com>
2020-05-11 16:40:11 +0000

    Aufgabe 3.1

--- poisson.cc
+++ poisson.cc
@@ -1,20 +1,39 @@
 #include <iostream>
 #include <fstream>
+#include <cmath> 
 #include <vector>
 
+using namespace std;
 
-double poisson(double mu, int k) {
-    return 0;
+vector<int> zaehler(11);
+int j = 0;
+double p, mu = 3.11538, e=2.71828;
+
+double poisson(int k) {
+
+  p = pow(mu,k)*pow(e,-mu)/(tgamma(k+1));
+  return p;
 }
 
 int main() {
-    using namespace std;
 
+  ifstream fin("datensumme.txt");
+  int n_i;
+  for(int i = 0 ; i < 234 ; ++i) {
 
-    ifstream fin("datensumme.txt");
-    int n_i;
-    for(int i = 0 ; i < 234 ; ++i) {
-        fin >> n_i;
-    }
-    fin.close();
+      fin >> n_i;
+      ++ zaehler[n_i]; //zaehler[n_i] += 1;
+
+  }
+  fin.close();
+
+  ofstream fout("histpoi.txt");
+  for(auto g : zaehler) {
+   
+    cout << g << endl;
+    fout << j << " " << g << " " << poisson(j) << endl;
+    ++j;
+  }
+
+  fout.close();
 }
