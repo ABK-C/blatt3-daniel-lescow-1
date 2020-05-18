03bad5dbe9380b5885048c5c6d7b2698f001d9dc
daniellescow <lescowdaniel@gmail.com>
2020-05-18 17:08:01 +0000

    Aufgabe 3.2

--- likelihood.cc
+++ likelihood.cc
@@ -1,20 +1,61 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cmath>
 
+using namespace std;
+double mu=3.11538;
 
-double poisson(double mu, int k) {
-    return 0;
+long double prob(vector<int> daten, double mu) {
+  
+  long double L = 1;
+  
+  for(int k : daten) {
+    L *= pow(mu, k) * exp(-mu) /(tgamma(k + 1));
+  }
+  return L;
 }
 
 int main() {
-    using namespace std;
+
+  vector<int> daten(0);
+
+  ifstream fin("datensumme.txt");
+  int n_i;
+  for(int i = 0 ; i < 234 ; ++i) {
+
+    fin >> n_i;
+    daten.push_back(n_i);
+
+  }
+  
+  fin.close();
+
+  cout << prob(daten , mu) << endl;
 
 
-    ifstream fin("datensumme.txt");
-    int n_i;
-    for(int i = 0 ; i < 234 ; ++i) {
-        fin >> n_i;
-    }
-    fin.close();
+  double i=0;
+  ofstream fout("likelihood.txt");
+  ofstream fout2("nll.txt");
+  ofstream fout3("deltall.txt");
+  do{ 
+    
+   fout << prob(daten,i) << endl;
+   fout2 << i << " " << -2*log(prob(daten,i)) << endl;
+   //Minimum bei 3.1 => min(L(mu))=893.926 Intervall ~[3 , 3.25]
+   fout3 << -2*log(prob(daten,mu))+2*log(prob(daten,i)) << endl;
+
+   i+=0.1;
+
+  } while(i<6);
+
+  fout.close();
+  fout2.close();
+  fout3.close();
+
+  for(int i = 0 ; i < 234 ; ++i) {
+
+    double A *= 
+
+  }
 }
